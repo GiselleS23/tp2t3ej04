5d9431b5a1be34dd80a84f76b8e14b1f1c32ddac
GiselleS23 <128999381+GiselleS23@users.noreply.github.com>
2024-05-10 20:57:01 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,33 @@
+/* 4.- Lea un valor entero positivo y compruebe si el número tiene la cifra 3 entre sus cifras. El codigo debe escribir ENCONTRADO, si contiene el 3 y NO ENCONTRADO en caso contrario.
+Sacchetti Maria Giselle C2*/
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Ingrese un número entero positivo:");
+        int numero = input.nextInt();
+
+        boolean encontrado = false;
+
+        
+        while (numero > 0) {
+            int digito = numero % 10;
+            if (digito == 3) {
+                encontrado = true;
+                break; 
+            }
+            numero /= 10;
+        }
+
+        if (encontrado) {
+            System.out.println("ENCONTRADO");
+        } else {
+            System.out.println("NO ENCONTRADO");
+        }
+
+        input.close();
+    }
+}
